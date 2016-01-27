50351560054efb2217a53d23892aac4cc8ab8757
wl33 <linewishuo@8afbfd1a.st-andrews.ac.uk>
2016-01-27 13:24:37 +0000

    Test NSDate

--- TestDate/main.m
+++ TestDate/main.m
@@ -12,6 +12,19 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        NSDate *date = [NSDate date];
+        NSLog(@"@date is %@",date);
+        
+//        date = [NSDate dateWithTimeIntervalSinceNow:10.0f];
+//        NSLog(@"@date is %@",date);
+        
+        //[NSThread sleepUntilDate:[NSDate dateWithTimeIntervalSinceNow:5]];
+        [NSThread sleepForTimeInterval:3];
+        
+        NSLog(@"slept for %f second",[[NSDate date] timeIntervalSinceDate:date]);
+        
+        
     }
     return 0;
 }
