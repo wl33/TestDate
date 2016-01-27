897dc64d89ff7ca9708f3f84ead1ac80af30ea21
wl33 <linewishuo@weishuos-macbook-pro.local>
2016-01-27 13:09:36 +0000

    Initial Commit

---
+++ TestDate/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestDate
+//
+//  Created by WEISHUO LIN on 27/01/2016.
+//  Copyright © 2016 WEISHUO LIN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
